9ff3b17cd109e47261dfdbfd8e5a0a91b8c34de4
Weston <w2visme@gmail.com>
2019-04-26 18:18:26 -0500

    Initial Commit

---
+++ Travelogue/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Travelogue
+//
+//  Created by Verhulst, Weston A. (MU-Student) on 4/26/19.
+//  Copyright © 2019 Verhulst, Weston A. (MU-Student). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
