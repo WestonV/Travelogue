8de7766910dc57555ba8500b4862ae33363eb768
Weston Verhulst <w2visme@gmail.com>
2019-05-01 22:50:32 -0500

    Added to storyboard

---
+++ Travelogue/DetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DetailViewController.swift
+//  Travelogue
+//
+//  Created by Weston Verhulst on 4/26/19.
+//  Copyright © 2019 Verhulst, Weston A. (MU-Student). All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
